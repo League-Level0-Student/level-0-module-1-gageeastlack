8099d5a555a456bdbba4bb786cb6ed0a15c42610
League Student <league@iMac.attlocal.net>
2019-08-06 18:29:15 -0700

    Day 2

---
+++ src/HappyUnbirthday.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class HappyUnbirthday {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog(null,"When is your Birthday?");
+	JOptionPane.showMessageDialog(null,"Nobody Cares."); 
+}
+}
