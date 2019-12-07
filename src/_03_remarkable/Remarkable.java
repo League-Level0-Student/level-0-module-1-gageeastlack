ab8b63d3de7727a96ccae6fcc513d0a00cce740f
League Student <league@iMac-5.attlocal.net>
2019-12-07 12:18:06 -0800

    The most boring experience of my life

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,12 +8,12 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-String gageisamazingateverything
+
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 	JOptionPane.showInputDialog(null,"What is your name?");
  
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+	JOptionPane.showMessageDialog(null,"You are not smart");
 	}
 }
 
--- src/_14_banana_split/banana_split/banana_split.pde
+++ src/_14_banana_split/banana_split/banana_split.pde
@@ -1,4 +1,7 @@
 void setup() {
   size(500,500);
-  text("banana", xPos, yPos);
+  text("banana", 230, 250);
+  text("ice cream", 230, 240);
+  text("ice cream", 230, 230);
+  text("ice cream", 230, 220);
 }
