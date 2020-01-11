83ac32dd954f1a9025e8d2d0a9800dd43e8153f9
League Student <league@iMac-5.attlocal.net>
2020-01-11 12:49:02 -0800

    :confused: :unamused:

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -14,14 +14,12 @@ import javax.swing.JOptionPane;
  * 
  */
 
-public class SecretMessageBox {
+public class 
 
 	// 0. Make a main method and put steps 1-5 inside it
-	public static void main(String[] args) {
-		
-	}
+	
 	// 1. Set a password in a String variable
-	void main(String["Gagalaga"]);
+
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
 
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
@@ -31,4 +29,4 @@ public class SecretMessageBox {
 
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
-}
+
--- src/myages.java
+++ src/myages.java
@@ -1,7 +1,7 @@
 
 public class myages {
 	public static void main(String[] args) {
-		for(int i = 0; i<13;i++) {
+		for(int i = 0; i<14;i++) {
 		System.out.println(i);
 		}
 	}
