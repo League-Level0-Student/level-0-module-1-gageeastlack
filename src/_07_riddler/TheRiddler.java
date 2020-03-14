b5fae9e24833dbf9de6da3719632e3dff7dbcc85
gageeastlack <51344312+gageeastlack@users.noreply.github.com>
2020-03-14 12:43:37 -0700

    Update TheRiddler.java

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -11,9 +11,12 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
 String scoreboard="score"
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-"What is greater than God, more evil than the devil, the poor have it, the rich need it, and if you eat it, you'll die?";
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if(guess.pass="score")
+String message = JOptionPane.showInputDialog(null, "What is greater than God, more evil than the devil, the poor have it, the rich need it, and if you eat it, you'll die?")
+	}	
+	// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if(guess.equals="score")
+	JOptionPane.showMessageDialog(null, "Correct"){
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 else
 	
